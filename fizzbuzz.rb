8bb460527ba1bc5e839ac0d35775bc808a5b5354
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 00:36:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(num)
-  if num % 3 == 0 
-    if num % 5 == 0 
-      "FizzBuzz"
-    else
-      "Fizz"
+  if (num % 3 == 0)
+    if (num % 5 == 0) 
+      "FizzBuzz";
+  
+  else
+      "Fizz";
+  
       
   elsif num % 5 == 10 
     "Buzz"
