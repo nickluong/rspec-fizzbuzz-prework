02f17e36a33d9d53774b5b493b00bee284d24dea
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 00:35:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz(num)
   if num % 3 == 0 
-    
+    if num % 5 == 0 
+      "FizzBuzz"
+    else
+      "Fizz"
+      
+  elsif num % 5 == 10 
+    "Buzz"
+  else 
+    nil 
+  end 
+end 
